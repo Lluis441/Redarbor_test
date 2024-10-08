3488892bc9f0d9c4756b49e7de8bf461c79c061a
Lluís <lluisvega1@gmail.com>
2024-10-08 20:37:47 +0200

    dos

--- Raw_data.sql
+++ Raw_data.sql
@@ -4,10 +4,10 @@ select
 	,userid
 	,time
 
-into Redarbor_test.dbo.Analysis_test
+into Redarbor_test.dbo.Data
 from Redarbor_test.dbo.bad
 
-UNION ALL --junto todas las líneas porque se que no habrá duplicados. Si lo quisiera sin duplicados, usaría union.
+UNION ALL --junto todas las lï¿½neas porque se que no habrï¿½ duplicados. Si lo quisiera sin duplicados, usarï¿½a union.
 
 select
 	'good' as quality
